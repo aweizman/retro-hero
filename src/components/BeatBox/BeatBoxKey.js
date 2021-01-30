052fa19874f00429f6ccdadb71fe8df098a6087f
DavidLIUXLD <DaViD28106@outlook.com>
2021-01-30 15:21:06 -0500

    fix button color not toggling issue

--- src/components/BeatBox/BeatBoxKey.js
+++ src/components/BeatBox/BeatBoxKey.js
@@ -1,14 +1,13 @@
 import React, { Component, useEffect, useState } from 'react';
 import '../../App.css';
 import '../../../node_modules/nes.css/css/nes.css'
-import userEvent from '@testing-library/user-event';
 
 function BeatBoxKey() {
     const [clicked, toggleClicked] = useState(false);
     const handleClick = (event) => {
         toggleClicked(!clicked);
     }
-    return <button type = "button" onClick = {handleClick} className = {'grid-item nes-btn ${clicked ? "isPrimary" : ""}'}></button>
+    return <button type = "button" onClick = {handleClick} className = {'grid-item nes-btn' + (clicked ? ' is-success' : '')}></button>
 }
 
 export default BeatBoxKey;
