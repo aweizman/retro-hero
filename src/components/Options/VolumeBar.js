14ca4bcf263fcf582e004d966b6c12c307badbf5
Adam Weizman <adamweizman@gmail.com>
2021-01-30 18:39:25 -0500

    added random code to VolumeBAr.js

---
+++ src/components/Options/VolumeBar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import '../../App.css'
+
+function VolumeBar(){
+    return 
+    <div>
+        <p>test</p>
+    </div>
+    
+}
+export default VolumeBar;
