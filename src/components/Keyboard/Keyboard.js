3b22d20eade42b32414a77d71533a31adc50db18
Adam Weizman <adamweizman@gmail.com>
2021-01-30 17:08:09 -0500

    Revert "added button that calls function that plays sound"
    This reverts commit 1285296f0afcc578952d58184b3231f06a408a7d.

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,31 +1,12 @@
 import React, {useState} from 'react';
 import '../../App.css';
-// Import the react-native-sound module
-var Sound = require('react-native-sound');
- 
-// Enable playback in silence mode
-Sound.setCategory('Playback');
 
 const Keyboard = () => {
 
     let [responseObj, setResponseObj] = useState({});
 
-    function playSound() {
-        var C4 = new Sound('sounds/C-4.mp3', Sound.MAIN_BUNDLE, (error) => {
-            if (error) {
-              console.log('failed to load the sound', error);
-              return;
-            }
-        });
-
-    }
-
     return (
         <div className="Keyboard-layout">
-            <Button
-                title = "test button"
-                onPress={() => playSound()} 
-            />
         </div>
     )
 
