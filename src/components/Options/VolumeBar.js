010cf3c247005ad9799543504106178c2017c5f3
Adam Weizman <adamweizman@gmail.com>
2021-01-30 23:52:48 -0500

    fixed stuff

--- src/components/Options/VolumeBar.js
+++ src/components/Options/VolumeBar.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
 import '../../App.css'
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-  } from 'react-native';
-  import VolumeSlider from 'react-native-volume-slider'
 
 export default class VolumeBar extends Component {
     render() {
         return (
-          <div>
-            <p>test<p/>
-          <div/>
-        )
+          <div><p>test</p></div>
+        );
     }
 }
