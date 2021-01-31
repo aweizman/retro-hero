ec49523d9e1af76733911394ec8b4f51b31fc027
Zach Curran <curran.zach@gmail.com>
2021-01-30 22:35:27 -0500

    Added key pressing functionality

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import _ from 'lodash';
 import '../../App.css';
+import {NOTE_TO_KEY} from '../global/notes'
 
 class Key extends React.Component {
     sharpNote = (note) => { // Check length of name, if longer than 2 it is sharp
         return note.length > 2;
     }
+    keyPressed = (note, keysPressed) => {
+        return _.includes(keysPressed, NOTE_TO_KEY[note]);
+    }
 
     render() {  
         let keyName = "key";    
         const sharpNote = this.sharpNote(this.props.note);  // Checks if sharp
+        const keyPressed = this.keyPressed(this.props.note, this.props.keysPressed);
         if(sharpNote) { // If sharp, adds sharp to name
             keyName += " sharp";
+        }
+        if(keyPressed) {
+            keyName += " pressed";
         }
 
         let key;
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,11 +2,52 @@ import React, {useState} from 'react';
 import _ from 'lodash';
 import Key from '../Key/Key.js'
 import '../../App.css';
-import NOTES from '../global/notes.js'
+import {NOTES} from '../global/notes.js'
+import {KEY_TO_NOTE} from '../global/notes.js'
 
 class Keyboard extends React.Component {
     constructor(props) {    // Keyboard constructor
         super(props);
+        this.state = {
+            keysPressed: [],
+        };
+    }
+
+    playNote = (note) => {
+        if(!_.isEmpty(note)) {  // Ensure pressing a key
+            const noteSound = new Audio(document.getElementById(note).src);
+            noteSound.play();
+        }
+    }
+
+
+    handleKeyDown = (event) => {
+        if(event.repeat) {  // Check if repeating
+            return;
+        }
+        const key = event.key;
+        const newlyPressedKeys = [...this.state.keysPressed];
+        if(!newlyPressedKeys.includes(key)) {   // Make sure that its not already pressed
+            newlyPressedKeys.push(key);
+        }
+        this.setState({
+            keysPressed: newlyPressedKeys,
+        })
+        this.playNote(KEY_TO_NOTE[key]);
+    }
+
+    handleKeyUp = (event) => {
+        const index = this.state.keysPressed.indexOf(event.key);
+        if (index > -1) {
+            this.setState(state => ({   // Get index of pressed key
+                keysPressed: state.keysPressed.splice(index, 1) // Remove the key
+            }));
+        }
+    }
+
+    componentDidMount = () => {
+        window.addEventListener('keydown', this.handleKeyDown);
+        window.addEventListener('keyup', this.handleKeyUp);
     }
 
     render() {
@@ -15,13 +56,28 @@ class Keyboard extends React.Component {
                 <Key
                     key={index}
                     note={note}
+                    keysPressed = {this.state.keysPressed}
                 />
             );
         });
 
+        const mp3Files = _.map(NOTES, (note, index) => {
+            return (
+                <audio
+                    id={note}
+                    key={index}
+                    src={`../../../public/sounds/${note}.mp3`}
+                    />
+            );
+        });
+        
+
         return(
             <div className="keyboard">
                 {keys}
+                <div>
+                {mp3Files}
+                </div>
             </div>
         );
     }
