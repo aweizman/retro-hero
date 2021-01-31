8caaa24469e98140b2eecb5db58b34397d5bbfc1
Adam Weizman <adamweizman@gmail.com>
2021-01-30 23:07:10 -0500

    added sample volume bar code

--- src/components/Options/VolumeBar.js
+++ src/components/Options/VolumeBar.js
@@ -8,12 +8,32 @@ import {
   } from 'react-native';
   import VolumeSlider from 'react-native-volume-slider'
 
-function VolumeBar(){
-    return (
-    <div>
-        <p>test</p>
-    </div>
-    )
-    
+export default class VolumeBar extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <VolumeSlider
+                  thumbSize={{
+                    width: 8,
+                    height: 8
+                  }}
+                  thumbTintColor="rgb(146,146,157)"
+                  minimumTrackTintColor="rgb(146,146,157)"
+                  maximumTrackTintColor="rgba(255,255,255, 0.1)"
+                  showsRouteButton
+                />
+            </View>
+        )
+    }
 }
-export default VolumeBar;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 20,
+      backgroundColor: '#eee',
+    },
+  });
+  
+  AppRegistry.registerComponent('VolumeSlider', () => VolumeSliderExample);
