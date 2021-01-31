8d5c6661a7193e1990aeb98d86f381b5a1b562a8
Adam Weizman <adamweizman@gmail.com>
2021-01-30 19:01:36 -0500

    volume bar filler

--- src/components/Options/VolumeBar.js
+++ src/components/Options/VolumeBar.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import '../../App.css'
 
 function VolumeBar(){
-    return 
+    return (
     <div>
         <p>test</p>
     </div>
+    )
     
 }
 export default VolumeBar;
